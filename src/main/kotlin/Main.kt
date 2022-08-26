7985f2b1219a2d29ad0e290433d6112a0a26c67a
Akriolis <Akriolis@gmail.com>
2022-08-26 18:33:48 +0400

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main(){
+    // # Integers:
+    // Byte, Short, Int, Long
+
+    // # Floating points
+    // Float, Double
+
+    // # Booleans
+    // Boolean (true or false)
+
+    // # Characters and Strings
+    // Char and String
+
+    var myArray = arrayOf(1,2,3,4,5)
+    println(myArray[0])
+    //count always starts from 0
+    print(myArray.size)
+}
